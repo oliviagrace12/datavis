233d084fa11b45b50b452c671e295f8fe7904073
Olivia Chisman <olivia.chisman@gmail.com>
2021-04-27 23:32:55 -0500

    lab 3

---
+++ lab_3/demo/madeuptree.csv.js
@@ -0,0 +1,19 @@
+var madeupTreeCSVText = `size,path
+,body
+,body/head
+,body/trunk
+,body/limbs
+,body/limbs/arms
+,body/limbs/legs
+6,body/head/ears
+5,body/head/nose
+100,body/head/brain
+500,body/trunk/thorax
+550,body/trunk/abdomen
+40,body/limbs/arms/forearm
+80,body/limbs/arms/bicep
+18,body/limbs/arms/hand
+180,body/limbs/legs/quad
+180,body/limbs/legs/hamstring
+90,body/limbs/legs/calf
+65,body/limbs/legs/foot`
