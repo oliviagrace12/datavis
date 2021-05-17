20cc7e1f1f0bffbcec3a16c21113ca51cd389857
Olivia Chisman <olivia.chisman@gmail.com>
2021-05-17 12:54:16 -0700

    data parsing extracted

--- lab_4/assignment/stackedbar.js
+++ lab_4/assignment/stackedbar.js
@@ -4,7 +4,7 @@
         this.SbDispatch = d3.dispatch("clicked");
     }
 
-    drawLineGraph() {
+    drawLineGraph(subgroups, groups, stackedData) {
         var margin = { top: 10, right: 30, bottom: 20, left: 50 },
             width = 460 - margin.left - margin.right,
             height = 400 - margin.top - margin.bottom;
@@ -20,58 +20,40 @@
 
         var dispatch = this.SbDispatch;
 
-        d3.csv("https://raw.githubusercontent.com/oliviagrace12/datavis/main/lab_4/assignment/fruit_data.csv", function (data) {
 
-            // List of subgroups = header of the csv files = soil condition here (Nitrogen, normal, stress)
-            var subgroups = data.columns.slice(1)
+        var x = d3.scaleBand()
+            .domain(groups)
+            .range([0, width])
+            .padding([0.2])
 
-            // List of groups = species here = value of the first column called group -> I show them on the X axis
-            var groups = d3.map(data, function (d) { return (d.group) }).keys()
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x).tickSizeOuter(0));
 
-            // Add X axis
-            var x = d3.scaleBand()
-                .domain(groups)
-                .range([0, width])
-                .padding([0.2])
+        var y = d3.scaleLinear()
+            .domain([0, 60])
+            .range([height, 0]);
+        svg.append("g")
+            .call(d3.axisLeft(y));
 
-            svg.append("g")
-                .attr("transform", "translate(0," + height + ")")
-                .call(d3.axisBottom(x).tickSizeOuter(0));
+        var color = d3.scaleOrdinal()
+            .domain(subgroups)
+            .range(['#e41a1c', '#377eb8', '#4daf4a'])
 
-            // Add Y axis
-            var y = d3.scaleLinear()
-                .domain([0, 60])
-                .range([height, 0]);
-            svg.append("g")
-                .call(d3.axisLeft(y));
-
-            // color palette = one color per subgroup
-            var color = d3.scaleOrdinal()
-                .domain(subgroups)
-                .range(['#e41a1c', '#377eb8', '#4daf4a'])
-
-            //stack the data? --> stack per subgroup
-            var stackedData = d3.stack()
-                .keys(subgroups)
-                (data)
-
-            // Show the bars
-            svg.append("g")
-                .selectAll("g")
-                .data(stackedData)
-                .enter().append("g")
-                .attr("fill", function (d) { return color(d.key); })
-                .on("click", function (d) {
-                    dispatch.call("clicked", {}, d.key);
-                })
-                .selectAll("rect")
-                .data(function (d) { return d; })
-                .enter().append("rect")
-                .attr("x", function (d) { return x(d.data.group); })
-                .attr("y", function (d) { return y(d[1]); })
-                .attr("height", function (d) { return y(d[0]) - y(d[1]); })
-                .attr("width", x.bandwidth());
-
-        })
+        svg.append("g")
+            .selectAll("g")
+            .data(stackedData)
+            .enter().append("g")
+            .attr("fill", function (d) { return color(d.key); })
+            .on("click", function (d) {
+                dispatch.call("clicked", {}, d.key);
+            })
+            .selectAll("rect")
+            .data(function (d) { return d; })
+            .enter().append("rect")
+            .attr("x", function (d) { return x(d.data.group); })
+            .attr("y", function (d) { return y(d[1]); })
+            .attr("height", function (d) { return y(d[0]) - y(d[1]); })
+            .attr("width", x.bandwidth());
     }
 }
