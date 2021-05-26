beb14c3c5f33e77c3329d497611ca9c0b363e71f
Chisman, Olivia (e23478) <olivia.chisman@cmegroup.com>
2021-05-26 18:58:43 -0500

    initial commit

---
+++ final_project/disorder-totals.csv.js
@@ -0,0 +1,2 @@
+var disorderTotalsText = `Disorder,Anxiety Disorder (Generalized, Social, Phobia, etc) total,Mood Disorder (Depression, Bipolar Disorder, etc) total,Attention Deficit Hyperactivity Disorder total,Post-traumatic Stress Disorder total,Personality Disorder (Borderline, Antisocial, Paranoid, etc) total,Obsessive-Compulsive Disorder  total,Substance Use Disorder total,Addictive Disorder total,Eating Disorder (Anorexia, Bulimia, etc) total
+Value,373,526,134,70,28,40,26,15,18`;
