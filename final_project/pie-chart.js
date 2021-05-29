c834417afda2af7c3ad64c276a4c353c4e2a6a2a
Chisman, Olivia (e23478) <olivia.chisman@cmegroup.com>
2021-05-28 20:50:43 -0500

    Pie chart title

--- final_project/pie-chart.js
+++ final_project/pie-chart.js
@@ -9,7 +9,7 @@ class PieChartMaker {
         // set the dimensions and margins of the graph
         var width = 450;
         var height = 450;
-        var margin = 40;
+        var margin = 80;
 
         // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
         var radius = Math.min(width, height) / 2 - margin
@@ -21,6 +21,29 @@ class PieChartMaker {
             .attr("height", height)
             .append("g")
             .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+        // Adding Title
+        svg.append("text")
+            .attr("x", 0 - width / 2)
+            .attr("y", 0 - height / 2 + 11)
+            // .attr("text-anchor", "middle")
+            .style("font-size", "16px")
+            .text("Diagnoses: Anxiety and/or Mood Disorder Versus Other")
+
+        svg.append("text")
+            .attr("x", 0 - width / 2)
+            .attr("y", 0 - height / 2 + 35)
+            // .attr("text-anchor", "middle")
+            .style("font-size", "13px")
+            .text("Anxiety Disorder: Generalized, Social, Phobia, etc")
+
+            svg.append("text")
+            .attr("x", 0 - width / 2)
+            .attr("y", 0 - height / 2 + 50)
+            // .attr("text-anchor", "middle")
+            .style("font-size", "13px")
+            .text("Mood Disorder: Depression, Bipolar Disorder, etc")
+
 
         // Create dummy data
         var data = { "Anxiety and/or Mood Disorder": 632, "Other": 84 }
@@ -68,6 +91,6 @@ class PieChartMaker {
             .text(function (d) { return d.data.key })
             .attr("transform", function (d) { return "translate(" + arcGenerator.centroid(d) + ")"; })
             .style("text-anchor", "middle")
-            .style("font-size", 17)
+            .style("font-size", 15)
     }
 }
